90f396dd129f093b35dd88a75582d9e1641d8b37
KasAgg21 <aggkas21@gmail.com>
2025-06-22 21:23:04 +0530

    Add image domains to Next.js configuration for optimized image loading

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,6 +5,10 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [];
   },
+  images: {
+    domains: ["localhost", "127.0.0.1", "social-media-z48c.onrender.com"],
+  },
+  
 };
 
 export default nextConfig;
